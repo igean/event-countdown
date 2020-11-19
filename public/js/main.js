65762d3d1c3218dc411f148989e79cf69e0adedb
Gean <coder.gean@gmail.com>
2020-11-19 17:17:56 -0200

    design latyouh

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
 const card = document.querySelector('.event-item');
 const menu = document.querySelector('.menu')
+const bars = document.querySelector('.bars')
 
-card.addEventListener('click', () => {
-    card.style.border = "2px groove blue"
-    menu.style.transform = "translateX(-100%)"
-})
+
+
+if (window.matchMedia('(max-width:500px)').matches) {
+
+    card.addEventListener('click', () => {
+        menu.style.transform = "translateX(-100%)"
+    })
+        
+    bars.addEventListener('click', () => {
+        if (menu.style.transform == "translateX(-100%)") {
+            menu.style.transform = "translateX(0)"
+        }
+    })
+}
