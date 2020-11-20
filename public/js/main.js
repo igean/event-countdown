9efc507de79ab3fb4ff01626fdd1362c9893ff32
Gean <coder.gean@gmail.com>
2020-11-20 15:33:07 -0200

    counter working

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@ const menu = document.querySelector('.menu')
 const bars = document.querySelector('.bars')
 
 
-
 if (window.matchMedia('(max-width:500px)').matches) {
 
     card.addEventListener('click', () => {
@@ -16,3 +15,27 @@ if (window.matchMedia('(max-width:500px)').matches) {
         }
     })
 }
+
+
+
+const countdown = document.querySelector('.countdown-time p')
+var target_date = new Date('2020-12-23 14:30:00')
+var days, hours, min, sec
+
+setInterval(() => {
+
+    var current_date = new Date()
+    var sec_counter = (target_date - current_date)/1000
+
+    days = parseInt(sec_counter/86400)
+    sec_counter = sec_counter%86400
+
+    hours = parseInt(sec_counter/3600)
+    sec_counter = sec_counter%3600
+
+    min = parseInt(sec_counter/60)
+    sec = parseInt(sec_counter%60)
+
+    countdown.innerText = days+'d:'+hours+'h:'+min+'m:'+sec+'s'
+
+}, 1000)
