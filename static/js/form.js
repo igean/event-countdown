049f474c3eff094a2523505f6f5f86aa60f48d66
Gean Araujo <coder.gean@gmail.com>
2020-11-25 13:53:54 -0300

    post method

--- static/js/form.js
+++ static/js/form.js
@@ -3,6 +3,6 @@ const inputs = document.querySelectorAll('.input')
 
 inputs.forEach(e => {
     e.children[1].addEventListener('focus', () => {
-        e.children[0].style.transform = 'translateY(-80%)'
+            e.children[0].style.transform = 'translateY(-80%)'
     })
 });
