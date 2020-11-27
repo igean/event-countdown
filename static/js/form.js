398550a1a34c9b3d317621cf522b36368111f2f0
Gean Araujo <coder.gean@gmail.com>
2020-11-27 12:26:55 -0300

    inputs

--- static/js/form.js
+++ static/js/form.js
@@ -1,8 +1,18 @@
 const inputs = document.querySelectorAll('.input')
+const container = document.querySelector('.container')
 
 
 inputs.forEach(e => {
-    e.children[1].addEventListener('focus', () => {
+    e.children[1].addEventListener('keydown', () => {
             e.children[0].style.transform = 'translateY(-80%)'
     })
+
 });
+
+container.addEventListener('click', () => {
+    inputs.forEach(e => {
+        if (e.children[1].value.length  < 1) {
+            e.children[0].style.transform = 'translateY(0)'
+        }
+    })
+})
