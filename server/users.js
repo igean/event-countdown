8aadbb2bc022d496dacfa75d7757aae24be99918
Gean Araujo <coder.gean@gmail.com>
2020-11-28 17:03:34 -0300

    server

---
+++ server/users.js
@@ -0,0 +1,19 @@
+const {Pool} = require('pg')
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+pool.connect()
+
+pool.query(`
+    CREATE TABLE users (
+        id NUMBER SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(200),
+        password VARCHAR(200),
+    )
+`)
