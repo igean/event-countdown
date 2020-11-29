4e2d4ec801246fb187c092a80ec8bd549bf538d4
Gean Araujo <coder.gean@gmail.com>
2020-11-29 17:55:26 -0300

    hash test

--- server/test.js
+++ server/test.js
@@ -1,4 +1,5 @@
 const {Pool} = require('pg')
+const bcrypt = require('bcrypt')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -9,8 +10,12 @@ const pool = new Pool({
 
 pool.connect().then(pool => {
     pool.query(`
-        SELECT * FROM users;
-    `).then (
-        pool => {console.log(pool.rows)}
+        SELECT password FROM users WHERE id = 3;
+    `).then(pass => {
+        pool.query(`UPDATE users SET password='${bcrypt.hash(pass,10)}'`)
+    }).then(
+        pool.query(`
+            SELECT * FROM users;
+        `).then(pool => {console.log(pool)})
     )
 })
