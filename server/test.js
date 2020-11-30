2b51d74a8877bfa8c07fd0fe9d9b7aae99b9cdaa
Gean Araujo <icrazy.gean@gmail.com>
2020-11-30 16:06:26 -0300

    ak

--- server/test.js
+++ server/test.js
@@ -13,7 +13,8 @@ pool.connect().then(
         if (results.rows.length < 1) {
             console.log('Usuário não encontrado')
         }else {
-            const b = bcrypt.compare('23012020',results.rows[0].password)
-            console.log(b)
+            bcrypt.compare('23012020',results.rows[0].password).then(r => {
+                console.log(r)
+            })
         }}) 
 )
