59b6d5b165fe27bbb5946a6b9fe5f1db37b56b47
Gean Araujo <icrazy.gean@gmail.com>
2020-12-04 23:12:06 -0300

    knfak

--- static/js/teste.js
+++ static/js/teste.js
@@ -1,18 +1,35 @@
 function start_countdown() {
     var title = document.querySelector('#title').value
-
-    let day = document.querySelector('#date').value.split('-')
-    day = day[2] + '/' + day[1] + '/' + day[0]
-
-    var hour = document.querySelector('#time').value
     var description = document.querySelector('#description').value
 
-    const container = document.querySelector('main')
+    let day_i = document.querySelector('#date').value.split('-')
+    var hour = document.querySelector('#time').value.split(':')
 
-    container.innerHTML = `
-        <h1>${title}</h1>
-        <h2>${day}</h2>
-        <h2>${hour}</h2>
-        <p>${description}</p>
-    `
+        day = new Date(day_i[0],day_i[1]-1,day_i[2],hour[0],hour[1])
+
+        const m = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+
+    setInterval(() => {
+        // counter
+        const now = new Date()
+        var time = (day - now)/1000
+        const days = parseInt(time/86400)
+    
+            time = time % 84600
+        const hour = parseInt(time/3600)
+    
+            time = time % 3600
+        const min = parseInt(time/60)
+    
+        const sec = parseInt(time%60)
+    
+        const container = document.querySelector('main')
+    
+        container.innerHTML = `
+            <h1>${title}</h1>
+            <strong>${days}d : ${hour}h : ${min}min : ${sec}s para o dia ${day_i[2]} de ${m[day_i[1]-1]} de ${day_i[0]}</strong>
+            <p>${description}</p>
+        `
+    }, 1000)
 }
