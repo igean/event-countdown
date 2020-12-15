34854fa4120c541559dae0f849b9be5bc84e28cf
Gean Araujo <icrazy.gean@gmail.com>
2020-12-15 19:17:24 -0300

    index code adjustment

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const express = require('express');
-const path = require('path')
-const hbs = require('hbs')
-const bodyParser = require('body-parser')
-const urlParser = bodyParser.urlencoded({extended: false})
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const urlParser = bodyParser.urlencoded({ extended: false });
 
-const app = express()
+const app = express();
 
-const controllers = require('./server/users')
+const controllers = require("./server/users");
 
 app
-.set('view engine', 'hbs')
-.set('views', path.join(__dirname, 'views'))
+ // essencial configs
+  .set("view engine", "hbs")
+  .set("views", path.join(__dirname, "views"))
+  .use(express.static(path.join(__dirname, "static")))
 
-.use(express.static(path.join(__dirname, 'static')))
+//   get routes
+  .get("/", (req, res) => {
+    return res.render("index");
+  })
+  .get("/cadastro", (req, res) => {
+    return res.render("cadastro");
+  })
+  .get("/login", (req, res) => {
+    return res.render("login");
+  })
+  .get("/teste", (req, res) => {
+    res.render("teste");
+  })
 
-.get('/', (req, res) => {
-    return res.render('index')
-})
+//   post routes
+  .post("/cadastro", urlParser, controllers.register)
+  .post("/login", urlParser, controllers.login);
 
-.get('/cadastro', (req, res) => {
-    return res.render('cadastro')
-})
-
-.get('/login', (req, res) => {
-    return res.render('login')
-})
-
-.get('/teste', (req, res) => {
-    res.render('teste')
-})
-
-.post('/cadastro', urlParser, controllers.register)
-
-.post('/login', urlParser, controllers.login)
-
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000);
